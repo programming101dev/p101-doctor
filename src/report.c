3037bb174ac08003bcabe5c30d91f32b6d532bc4
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-07-29 14:41:44 -0700

    Use bounded JSON serialization

--- src/report.c
+++ src/report.c
@@ -113,6 +113,7 @@ static void trim_newline(const struct p101_env *env, char *line)
 static void write_json_string(const struct p101_env *env, struct p101_error *err, FILE *stream, const char *text)
 {
     const unsigned char *cursor;
+    size_t               length;
 
     p101_fputc(env, err, '\"', stream);
 
@@ -122,35 +123,37 @@ static void write_json_string(const struct p101_env *env, struct p101_error *err
     }
 
     cursor = (const unsigned char *)text;
-    while(*cursor != '\0' && p101_error_has_no_error(err))
+    length = p101_strlen(env, text);
+    for(size_t index = 0U; index < length && p101_error_has_no_error(err); index++)
     {
-        if(*cursor == '\"' || *cursor == '\\')
+        unsigned char current;
+
+        current = cursor[index];
+        if(current == '\"' || current == '\\')
         {
             p101_fputc(env, err, '\\', stream);
-            p101_fputc(env, err, (int)*cursor, stream);
+            p101_fputc(env, err, (int)current, stream);
         }
-        else if(*cursor == '\n')
+        else if(current == '\n')
         {
             p101_fputs(env, err, "\\n", stream);
         }
-        else if(*cursor == '\r')
+        else if(current == '\r')
         {
             p101_fputs(env, err, "\\r", stream);
         }
-        else if(*cursor == '\t')
+        else if(current == '\t')
         {
             p101_fputs(env, err, "\\t", stream);
         }
-        else if(*cursor < JSON_CONTROL_BYTE_LIMIT || *cursor >= JSON_NON_ASCII_BYTE_BASE)
+        else if(current < JSON_CONTROL_BYTE_LIMIT || current >= JSON_NON_ASCII_BYTE_BASE)
         {
-            p101_fprintf(env, err, stream, "\\u%04x", (unsigned)*cursor);
+            p101_fprintf(env, err, stream, "\\u%04x", (unsigned)current);
         }
         else
         {
-            p101_fputc(env, err, (int)*cursor, stream);
+            p101_fputc(env, err, (int)current, stream);
         }
-
-        cursor++;
     }
 
 done:
